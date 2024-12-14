bbea0fe170ef6a1bcfb954b40cba56d698be4671
Tuan Kiet <Tuankie102@gmail.com>
2024-12-14 20:27:24 +0700

    ejs

--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const port = 8081
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get('/test', (req, res) => {
-    res.send('Just make a test!')
+    res.render('example.ejs')
 })
 
 
