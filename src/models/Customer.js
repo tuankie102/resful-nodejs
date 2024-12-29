2c4bcc4f9621ad646a65f5ef4483d2fbea3a3fb4
Tuan Kiet <Tuankie102@gmail.com>
2024-12-29 11:48:04 +0700

    create custumer model

---
+++ src/models/Customer.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    address: String,
+    phone: String,
+    email: String,
+    image: String,
+    description: String,
+}, { timestamps: true });
+const Customer = mongoose.model("user", customerSchema);
+
+module.exports = Customer;
